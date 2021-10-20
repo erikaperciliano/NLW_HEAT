ef6a692bb59d60c35438c8a5506d26732b058742
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-10-19 23:32:53 -0300

    filtering user information

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -13,6 +13,13 @@ interface IAccessTokenResponse {
     access_token: string;
 }
 
+interface IUserResponse {
+    avatar_url: string;
+    login: string;
+    id: number;
+    name: string;
+}
+
 class AuthenticateUserService {
     async execute(code: string) {
         const url = 'https://github.com/login/oauth/access_token';
@@ -28,7 +35,7 @@ class AuthenticateUserService {
             }
         });
 
-        const response = await axios.get('https://api.github.com/user', {
+        const response = await axios.get<IUserResponse>('https://api.github.com/user', {
             headers: {
                 authorization: `Bearer ${accessTokenResponse.access_token}`
             }
