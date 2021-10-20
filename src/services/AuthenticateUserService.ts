431472e840cc1dc9cc940488b30c1ca334059456
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-10-20 00:08:06 -0300

    create token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import prismaClient from '../prisma';
+import { sign } from 'jsonwebtoken';
+
 
 /**
  * Receber code(string)
@@ -62,7 +64,22 @@ class AuthenticateUserService {
             })
         }
 
-        return response.data;
+        const token = sign({
+            user: {
+                name: user.name,
+                avatar_url: user.avatar_url,
+                id: user.id
+            }
+        },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '1d'
+            }
+
+        ) 
+
+        return { token, user }
     }
 }
 
