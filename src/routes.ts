dbf7e2897e67afa2a77a6fa259615d7391ed2f2c
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-10-21 21:35:04 -0300

    returning profile of users

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserContoller';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { Get3LastMessagesController } from './controllers/GetLast3MessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
@@ -10,5 +11,6 @@ router.post('/authenticate', new AuthenticateUserController().handle);
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
 
 router.get('/messages/last3', new Get3LastMessagesController().handle);
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
 
 export { router };
